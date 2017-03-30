fe90e6298cab5dde8543ca450ffbe618f1ff5263
Aaron Greenwald <aarong@wix.com>
2017-03-30 13:03:08 +0300

    failing detox test - why??

--- test/e2e/sanity-e2e.js
+++ test/e2e/sanity-e2e.js
@@ -8,14 +8,14 @@ describe('sanity e2e', () => {
     await expect(element(by.id('list'))).toBeVisible();
   });
 
-  // it('should see more messages after scrolling', async () => {
-  //   await expect(element(by.id('0'))).toBeVisible();
-  //   await expect(element(by.id('19'))).toBeNotVisible();
-  //   await element(by.id('list')).swipe('up', 'fast');
-  //   await expect(element(by.id('0'))).toBeNotVisible();
-  //   await expect(element(by.id('19'))).toBeVisible();
-  //
-  // });
+  it('should see more messages after scrolling', async () => {
+    await expect(element(by.id('0'))).toBeVisible();
+    await expect(element(by.id('19'))).toBeNotVisible();
+    await element(by.id('list')).swipe('up', 'fast');
+    await expect(element(by.id('0'))).toBeNotVisible();
+    await expect(element(by.id('19'))).toBeVisible();
+
+  });
   //
   //
   // it('should not render too many messages at once', async () => {
